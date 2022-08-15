36ff85a19f809134691d400a681cd06be1c3c858
YoonDaeSung <wkzks123@naver.com>
2022-08-15 23:30:01 +0900

    디테일뷰 세팅 전 ui설정 완료

---
+++ Diary_FC/WriteDiaryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WriteViewController.swift
+//  Diary_FC
+//
+//  Created by YoonDaeSung on 2022/08/15.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
